5bab40edf8e72c494729a20c7f1f88ba952edb53
Fernando <fernandowashimi@gmail.com>
2019-04-07 02:39:27 -0300

    Changed structure and fetching method

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Segment, Input, Dropdown, Button, Icon } from "semantic-ui-react";
-import api from "../services/api";
 
 const options = [
   { key: "pc", text: "PC", value: "pc" },
@@ -12,53 +11,36 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      name: "",
-      platform: "pc",
-      loading: false
-    };
+    this.state = {};
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleDropdownChange = this.handleDropdownChange.bind(this);
-    this.fetchData = this.fetchData.bind(this);
   }
 
+  // Update input param in state and send to parent
   handleInputChange(e) {
-    this.setState({ name: e.target.value });
+    this.props.sendInputParam(e.target.value);
+    // this.setState({ searchParams: { name: e.target.value } }, () => {
+    //   this.props.sendInputParam(this.state.searchParams.name);
+    // });
   }
 
+  // Update dropdown param in state and send to parent
   handleDropdownChange(e, { value }) {
-    this.setState({ platform: value });
-  }
-
-  async fetchData() {
-    this.setState({ loading: true });
-    this.props.displaySegment();
-
-    try {
-      const response = await api.get("search.php", {
-        params: {
-          platform: this.state.platform,
-          search: this.state.name
-        }
-      });
-      this.props.sendData(response.data);
-      console.log(this.state.data);
-    } catch (error) {
-      console.warn(error);
-    } finally {
-      this.setState({ loading: false });
-    }
+    this.props.sendDropdownParam(value);
+    // this.setState({ searchParams: { platform: value } }, () => {
+    //   this.props.sendDropdownParam(this.state.searchParams.platform);
+    // });
   }
 
   render() {
-    const { platform } = this.state;
     return (
       <div>
         <Segment
           inverted
           style={{
-            marginTop: "4em"
+            marginTop: "4em",
+            marginBottom: "1em"
           }}
         >
           <Input
@@ -68,22 +50,17 @@ class SearchBar extends Component {
             action
             fluid
           >
-            <input
-              onChange={e => {
-                this.handleInputChange(e);
-              }}
-            />
+            <input onChange={this.handleInputChange} />
             <Dropdown
               button
               options={options}
-              value={platform}
+              defaultValue={"pc"}
               onChange={this.handleDropdownChange}
             />
             <Button
               animated="vertical"
               color="grey"
-              onClick={this.fetchData}
-              loading={this.state.loading}
+              onClick={this.props.fetchData}
             >
               <Button.Content hidden>Procurar</Button.Content>
               <Button.Content visible>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,100 @@
 import React, { Component } from "react";
+import api from "../services/api";
 import Footer from "../components/Footer";
 import SearchBar from "../components/SearchBar";
 import Head from "../components/Head";
-import { Grid, Transition, Segment } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
-      data: {
-        totalresults: 0
-      },
-      visible: false
+      searchParams: {
+        platform: "pc", // Default paltform
+        name: ""
+      }
     };
 
-    this.sendData = this.sendData.bind(this);
-    this.handleVisibility = this.handleVisibility.bind(this);
+    // this.sendData = this.sendData.bind(this);
+    // this.handleStatus = this.handleStatus.bind(this);
+    // this.handleVisibility = this.handleVisibility.bind(this);
+    // this.teste = this.teste.bind(this);
+    this.getInputParam = this.getInputParam.bind(this);
+    this.getDropdownParam = this.getDropdownParam.bind(this);
+    this.fetchData = this.fetchData.bind(this);
   }
 
-  sendData(data) {
-    this.setState({ data: data });
+  // Get input value from SearchBar component
+  getInputParam(name) {
+    this.setState({
+      searchParams: {
+        name,
+        platform: this.state.searchParams.platform
+      }
+    });
   }
 
-  handleVisibility() {
-    if (!this.state.visible) {
-      this.setState({ visible: !this.state.visible });
+  // Get dropdown value from SearchBar component
+  getDropdownParam(platform) {
+    this.setState({
+      searchParams: {
+        platform,
+        name: this.state.searchParams.name
+      }
+    });
+  }
+
+  // Start search proccess
+  async fetchData() {
+    try {
+      // Get the basic info using the player name (the plyer aid is necessary to get the full stats)
+      const basic_info = await api.get("search.php", {
+        params: {
+          platform: this.state.searchParams.platform,
+          search: this.state.searchParams.name
+        }
+      });
+
+      // Check if the request has a result
+      if (basic_info.data.totalresults) {
+        try {
+          // Get the full info using the aid received from the first request
+          const full_info = await api.get("player.php", {
+            params: {
+              aid: basic_info.data.results[0].aid
+            }
+          });
+
+          // Update state with the result
+          this.setState({ playerInfo: full_info.data });
+        } catch (error) {
+          console.warn(error);
+        }
+      } else {
+        this.setState({
+          playerInfo: undefined
+        });
+      }
+    } catch (error) {
+      console.warn(error);
+    } finally {
+      console.log(this.state);
     }
   }
+
+  // sendData(data) {
+  //   this.setState({ data });
+  // }
+
+  // handleStatus(loading) {
+  //   this.setState({ loading });
+  // }
+
+  // handleVisibility() {
+  //   if (!this.state.visible) {
+  //     this.setState({ visible: !this.state.visible });
+  //   }
+  // }
 
   render() {
     return (
@@ -51,18 +119,10 @@ class Home extends Component {
           <Grid.Column style={{ maxWidth: "800px", margin: "1em" }}>
             <Head />
             <SearchBar
-              sendData={this.sendData}
-              displaySegment={this.handleVisibility}
+              sendInputParam={this.getInputParam}
+              sendDropdownParam={this.getDropdownParam}
+              fetchData={this.fetchData}
             />
-            <Transition.Group animation={"slide down"} duration={500}>
-              {this.state.visible && (
-                <Segment>
-                  {this.state.data.totalresults
-                    ? JSON.stringify(this.state.data.results, null, 2)
-                    : "loading"}
-                </Segment>
-              )}
-            </Transition.Group>
             <Footer />
           </Grid.Column>
         </Grid>
